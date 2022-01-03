354a5f9f97e40730e18e87864df295a171d234d7
Shorayn <34161708+Shorayn@users.noreply.github.com>
2022-01-03 20:04:00 +0100

    removed repository in show since slug in wildcard is same as slug in enitity

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -73,22 +73,11 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
+    public function show(Question $question)
     {
         if ($this->isDebug) {
             $this->logger->info('We are in debug mode!');
         }
-
-
-        $repository = $entityManager->getRepository(Question::class);
-        /** @var Question|null $question  */
-        $question = $repository->findOneBy(['slug'=> $slug]);
-
-        if(!$question){
-            throw $this->createNotFoundException(sprintf('no question found for %s', $slug));
-        }
-        
-
 
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still 🤣',
